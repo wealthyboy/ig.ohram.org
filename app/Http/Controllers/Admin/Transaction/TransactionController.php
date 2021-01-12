0ea5a078eab8d8d19e6fbe8162ef39b8546b9961
Jacob Atam <jacob.atam@gmail.com>
2021-01-12 03:24:01 +0100

    dumping products

--- app/Http/Controllers/Admin/Transaction/TransactionController.php
+++ app/Http/Controllers/Admin/Transaction/TransactionController.php
@@ -85,6 +85,8 @@ class TransactionController extends Controller
 				// Show me the result
                 $json = json_decode($data, TRUE);
                 curl_close($ch);    //END CURL SESSION///////////////////////////////
+                dd($json);
+
                     $transaction_log->transaction_reference = $json["MerchantReference"];
                     $transaction_log->approved_amount = $json["Amount"] / 100;
                     $transaction_log->response_description = $json["ResponseDescription"];
