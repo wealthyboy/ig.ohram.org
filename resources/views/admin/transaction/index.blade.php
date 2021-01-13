b3a49ca2f33b04f3ec156a4d747ac0d6d4e06d5b
Jacob Atam <jacob.atam@gmail.com>
2021-01-13 05:28:38 +0100

    Dumped response

--- resources/views/admin/transaction/index.blade.php
+++ resources/views/admin/transaction/index.blade.php
@@ -17,74 +17,77 @@
                         <!-- Here you can write extra buttons/actions for the toolbar              -->
                     </div>
                     <div class="material-datatables">
-                    <form action="/admin/products/destroy/multiple" method="post" enctype="multipart/form-data" id="form-products">
-                        @method('DELETE')
-                        @csrf
-                
-                        <table id="datatables" class="table table-striped table-shopping table-no-bordered table-hover" cellspacing="0" width="100%" style="width:100%">
-                            <thead>
+                        <form action="/admin/products/destroy/multiple" method="post" enctype="multipart/form-data" id="form-products">
+                            @method('DELETE')
+                            @csrf
 
-                                <tr>
-                                  <th>
-                                    <div class="checkbox">
-                                        <label>
-                                            <input onclick="$('input[name*=\'selected\']').prop('checked', this.checked);" type="checkbox" name="optionsCheckboxes" >
-                                        </label>
-                                    </div>
-                                    </th>
-                                    <th>Transaction id</th>
+                            <div class="table-responsive">
+                    
+                            <table id="datatables" class="table table-striped table-shopping table-no-bordered table-hover" cellspacing="0" width="100%" style="width:100%">
+                                <thead>
 
-                                    <th>Full Name</th>
-                                    <th>Transaction Reference</th>
-                                    <th>Amount</th>
-                                    <th>Approved Amount</th>
-                                    <th>Response Description</th>
-                                    <th>Status</th>
-                                    <th>Response Code</th>
-                                    <th>Response DateTime</th>
-                                    <th class="disabled-sorting text-right">Actions</th>
-                                </tr>
-                            </thead>
-                            
-                            <tbody>
-                                @if($transactions->count())
-                                    @foreach($transactions as $transaction)
-                                        <tr>
-                                            <td>
-                                                <div class="checkbox">
-                                                    <label>
-                                                        <input type="checkbox" value="" name="selected[]" >
-                                                    </label>
-                                                </div>
-                                            </td>
-                                            <!-- cart-active -->
-                                            
-                                            <td >{{ $transaction->id }}</td>
-                                            <td>{{ optional($transaction->user)->fullname() }}</td>
-                                            <td class="text-center">{{ $transaction->transaction_reference }}</td>
-                                            <td class="text-center">{{ $transaction->amount }}</td>
-                                            <td class="text-center">{{ $transaction->approved_amount }} </td>
-                                            <td class="text-center">{{ $transaction->response_description }} </td>
-                                            <td class="text-center">{{ $transaction->status }} </td>
-                                            <td class="text-center">{{ $transaction->response_code }} </td>
-                                            <td class="text-center">{{ $transaction->response_date_time }}</td>
+                                    <tr>
+                                    <th>
+                                        <div class="checkbox">
+                                            <label>
+                                                <input onclick="$('input[name*=\'selected\']').prop('checked', this.checked);" type="checkbox" name="optionsCheckboxes" >
+                                            </label>
+                                        </div>
+                                        </th>
+                                        <th>Transaction id</th>
+
+                                        <th>Full Name</th>
+                                        <th>Transaction Reference</th>
+                                        <th>Amount</th>
+                                        <th>Approved Amount</th>
+                                        <th>Response Description</th>
+                                        <th>Status</th>
+                                        <th>Response Code</th>
+                                        <th>Response DateTime</th>
+                                        <th class="disabled-sorting text-right">Actions</th>
+                                    </tr>
+                                </thead>
+                                
+                                <tbody>
+                                    @if($transactions->count())
+                                        @foreach($transactions as $transaction)
+                                            <tr>
+                                                <td>
+                                                    <div class="checkbox">
+                                                        <label>
+                                                            <input type="checkbox" value="" name="selected[]" >
+                                                        </label>
+                                                    </div>
+                                                </td>
+                                                <!-- cart-active -->
+                                                
+                                                <td >{{ $transaction->id }}</td>
+                                                <td>{{ optional($transaction->user)->fullname() }}</td>
+                                                <td class="text-center">{{ $transaction->transaction_reference }}</td>
+                                                <td class="text-center">{{ $transaction->amount }}</td>
+                                                <td class="text-center">{{ $transaction->approved_amount }} </td>
+                                                <td class="text-center">{{ $transaction->response_description }} </td>
+                                                <td class="text-center">{{ $transaction->status }} </td>
+                                                <td class="text-center">{{ $transaction->response_code }} </td>
+                                                <td class="text-center">{{ $transaction->response_date_time }}</td>
 
 
 
-                                            <td></td>
-                                            <td class="td-actions ">                     
-                                                <a href="/admin/requery/{{ $transaction->id }}" rel="tooltip" title="Requery" class="btn btn-primary btn-simple btn-xs">
-                                                    Requery
-                                                </a>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                @else
+                                                <td></td>
+                                                <td class="td-actions ">                     
+                                                    <a href="/admin/requery/{{ $transaction->id }}" rel="tooltip" title="Requery" class="btn btn-primary btn-simple btn-xs">
+                                                        Requery
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    @else
 
-                                @endif
-                             
-                            </tbody>
-                         </table>
+                                    @endif
+                                
+                                </tbody>
+                            </table>
+                         </div>
                         </form>
                     </div>
                 </div><!-- end content-->
